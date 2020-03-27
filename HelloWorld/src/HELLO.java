aeff9034524ce06347092d92ff4ea0e84ca00453
rickicardoso <ricki_cardoso@hotmail.com>
2020-03-27 05:45:45 +0000

    junit e javadoc

---
+++ HelloWorld/src/HELLO.java
@@ -0,0 +1,14 @@
+
+public class HELLO {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HELLO");   
+	}
+	
+	public int works() {
+		System.out.println("HELLO");
+		return 1;
+	}
+
+}
---
+++ HelloWorld/src/test.java
@@ -0,0 +1,16 @@
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class test {
+
+	@Test
+	void test() {
+		HELLO teste = new HELLO();
+		int x = teste.works();
+		assertEquals(1, x);
+	}
+
+}
